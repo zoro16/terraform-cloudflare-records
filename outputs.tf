f40a45fa878d4b11318251bf6b5c4acb0ae49e8c
Mohamed Abdelgadir <mosaleh@tutanota.com>
2023-10-05 19:57:54 +0200

    chore: changed output description format

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,18 @@
 output "records" {
   description = <<EOF
-All records output:
+Records output:
 
+    * `created_on` (String) The RFC3339 timestamp of when the record was created.
+    * `hostname` (String) The FQDN of the record.
+    * `id` (String) The ID of this resource.
+    * `metadata` (Map of String) A key-value map of string metadata Cloudflare associates with the record.
+        ** `auto_added` (Boolean)
+        ** `managed_by_apps` (Boolean)
+        ** `managed_by_argo_tunnel` (Boolean)
+        ** `source` (String)
 
-    `created_on` (String) The RFC3339 timestamp of when the record was created.
-    `hostname` (String) The FQDN of the record.
-    `id` (String) The ID of this resource.
-    `metadata` (Map of String) A key-value map of string metadata Cloudflare associates with the record.
-        `auto_added` (Boolean)
-        `managed_by_apps` (Boolean)
-        `managed_by_argo_tunnel` (Boolean)
-        `source` (String)
-
-    `modified_on` (String) The RFC3339 timestamp of when the record was last modified.
-    `proxiable` (Boolean) Shows whether this record can be proxied.
+    * `modified_on` (String) The RFC3339 timestamp of when the record was last modified.
+    * `proxiable` (Boolean) Shows whether this record can be proxied.
 EOF
   value = {
     for k, v in cloudflare_record.this :
